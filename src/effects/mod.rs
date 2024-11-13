8e46cefbf375691331b53e468703a075867effc4
Jacob Birkett <jacob_birkett@fourier.earth>
2024-11-12 18:10:02 -0800

    effects: MapEffect: generate Effect impl with macro

--- src/effects/mod.rs
+++ src/effects/mod.rs
@@ -10,17 +10,41 @@ pub trait Effect {
     fn apply(&self, value: &Value) -> Result<Value>;
 }
 
-#[derive(Deserialize)]
-#[serde(tag = "kind")]
-pub enum MapEffect {
-    #[serde(rename = "truthy_keys")]
-    TruthyKeys(TruthyKeys),
+macro_rules! impl_map_effect_enum {
+    (
+        $( #[ $enum_meta:meta ] )*
+        $enum_vis:vis enum $EnumIdent:ident {
+            $(
+                $( #[ $variant_meta:meta ] )*
+                $VariantIdent:ident ( $VariantType:ty ) ,
+            )+
+        }
+    ) => {
+        $(#[$enum_meta])*
+        $enum_vis enum $EnumIdent {
+            $(
+                $(#[$variant_meta])*
+                $VariantIdent($VariantType),
+            )+
+        }
+
+        impl Effect for $EnumIdent {
+            fn apply(&self, value: &Value) -> serde_json::Result<Value> {
+                match self {
+                    $(
+                        $EnumIdent::$VariantIdent(effect) => effect.apply(value),
+                    )+
+                }
+            }
+        }
+    }
 }
 
-impl Effect for MapEffect {
-    fn apply(&self, value: &Value) -> Result<Value> {
-        match self {
-            Self::TruthyKeys(effect) => effect.apply(value),
-        }
+impl_map_effect_enum! {
+    #[derive(Deserialize)]
+    #[serde(tag = "kind")]
+    pub enum MapEffect {
+        #[serde(rename = "truthy_keys")]
+        TruthyKeys(TruthyKeys),
     }
 }
